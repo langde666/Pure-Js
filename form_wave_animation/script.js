62162ae0669fa3ecda642fa141ef52fbf3712c04
Nghia Dang <vannghiadang79@gmail.com>
2021-09-19 16:56:13 +0700

    form wave animation

---
+++ form_wave_animation/script.js
@@ -0,0 +1,11 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerHTML
+        .split('')
+        .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+        .join('');
+});
